508a471c6114d030adf90cb4f2d84d9f4c88bc2f
Zev Nevo <zev@nevo.io>
2022-04-27 13:23:33 -0400

    base contracts and deployment scripts

---
+++ scripts/deploy_Marketplace.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Marketplace = await hre.ethers.getContractFactory("Marketplace");
+  const marketplace = await Marketplace.deploy();
+
+  await marketplace.deployed();
+
+  console.log("Marketplace deployed to:", marketplace.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
