94dbd3b9fd77091be5562f465247f442d76a6e95
Zev Nevo <zev@nevo.io>
2022-04-27 20:04:52 -0400

    config updates for deployments

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require('solidity-coverage');
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -20,4 +21,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.4",
   plugins: ["solidity-coverage"],
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    fuji: {
+      url: process.env.FUJI_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
 };
--- scripts/deploy_Marketplace.js
+++ scripts/deploy_Marketplace.js
@@ -1,8 +1,10 @@
 const hre = require("hardhat");
 
 async function main() {
+  const lzEndpoint = "0x93f54D755A063cE7bB9e6Ac47Eccc8e33411d706";
+
   const Marketplace = await hre.ethers.getContractFactory("Marketplace");
-  const marketplace = await Marketplace.deploy();
+  const marketplace = await Marketplace.deploy(lzEndpoint);
 
   await marketplace.deployed();
 
